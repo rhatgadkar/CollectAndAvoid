6e4f4c817b9150a49b9bbbeb7e87decddecc12ce
Rishabh Hatgadkar <rhatgadkar@gmail.com>
2018-03-18 10:59:43 -0700

    Add unit tests for player food and wall collisions.

--- python-client/tests/test_player.py
+++ python-client/tests/test_player.py
@@ -15,7 +15,7 @@ class TestPlayer(unittest.TestCase):
         '''
         Player does not collide with any object and moves from default Keys.up.
         Verify that player variables are unchanged except y position.
-        
+
         Map:
         wwwwwwwwwwwwwwwwwwww
         w000000000000000000w
@@ -63,7 +63,7 @@ class TestPlayer(unittest.TestCase):
         Player collides with food from bottom, by moving up.
         Verify that player's got_food changes to True, y position changes, and
         score increments.
-        
+
         Map before collision:
         wwwwwwwwwwwwwwwwwwww
         w000000000000000000w
@@ -115,7 +115,7 @@ class TestPlayer(unittest.TestCase):
         '''
         Player collides with wall from right, by moving left.
         Verify that player's dead changes to True and x position changes.
-        
+
         Map before collision:
         wwwwwwwwwwwwwwwwwwww
         w000000000000000000w
@@ -165,6 +165,177 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(new_got_food, player.got_food)
         self.assertEqual(new_score, player.score)
 
+    def test_collide_from_top(self):
+        '''
+        Player collides with wall from top, by moving down.
+        Verify that player's dead changes to True and y position changes.
+
+        Map before collision:
+        wwwwwwwwwwwwwwwwwwww
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000f00000000w
+        w000000000p00000000w
+        w000000000w00000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        wwwwwwwwwwwwwwwwwwww
+        '''
+        Keys.reset()
+        food_row = 8
+        food_col = 10
+        world = World(food_row, food_col)
+        world.grid[10][10] = Wall(10, 10)
+        player = Player(world)
+        player_row = 9
+        player_col = 10
+        player_y = Tile.DIMS[1] * player_row
+        player_x = Tile.DIMS[0] * player_col
+        player.position = (player_x, player_y)
+        Keys.set_down()
+
+        new_x = player.position[0]
+        new_y = player.position[1] + Player.VERT_SPEED
+        new_dead = True
+        new_got_food = player.got_food
+        new_score = player.score
+
+        player.do_something()
+
+        self.assertEqual(new_x, player.position[0])
+        self.assertEqual(new_y, player.position[1])
+        self.assertEqual(new_dead, player.dead)
+        self.assertEqual(new_got_food, player.got_food)
+        self.assertEqual(new_score, player.score)
+
+    def test_collide_from_left(self):
+        '''
+        Player collides with food from left, by moving right.
+        Verify that player's got_food changes to True, x position changes, and
+        score increments.
+
+        Map before collision:
+        wwwwwwwwwwwwwwwwwwww
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w00000000pf00000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        wwwwwwwwwwwwwwwwwwww
+        '''
+        Keys.reset()
+        food_row = 10
+        food_col = 10
+        world = World(food_row, food_col)
+        player = Player(world)
+        player_row = 10
+        player_col = 9
+        player_y = Tile.DIMS[1] * player_row
+        player_x = Tile.DIMS[0] * player_col
+        player.position = (player_x, player_y)
+        Keys.set_right()
+
+        new_x = player.position[0] + Player.HORIZ_SPEED
+        new_y = player.position[1]
+        new_dead = player.dead
+        new_got_food = True
+        new_score = player.score + 1
+
+        player.do_something()
+
+        self.assertEqual(new_x, player.position[0])
+        self.assertEqual(new_y, player.position[1])
+        self.assertEqual(new_dead, player.dead)
+        self.assertEqual(new_got_food, player.got_food)
+        self.assertEqual(new_score, player.score)
+
+    def test_during_food_collision(self):
+        '''
+        Verify score does not increment during collision when got_food is
+        already True.  Verify that player's got_food remains True, y position
+        changes, and score remains the same.
+        '''
+        Keys.reset()
+        food_row = 10
+        food_col = 10
+        world = World(food_row, food_col)
+        player = Player(world)
+        player_row = 10
+        player_col = 10
+        player_y = Tile.DIMS[1] * player_row
+        player_x = Tile.DIMS[0] * player_col
+        player.position = (player_x, player_y)
+        player.got_food = True
+
+        new_x = player.position[0]
+        new_y = player.position[1] - Player.VERT_SPEED
+        new_dead = player.dead
+        new_got_food = player.got_food
+        new_score = player.score
+
+        player.do_something()
+
+        self.assertEqual(new_x, player.position[0])
+        self.assertEqual(new_y, player.position[1])
+        self.assertEqual(new_dead, player.dead)
+        self.assertEqual(new_got_food, player.got_food)
+        self.assertEqual(new_score, player.score)
+
+    def test_after_food_collision(self):
+        '''
+        Verify that got_food is set to False after a food collision is done and
+        player is in a tile space that does not contain anything.
+        Verify that the y position changes and score remains the same.
+        '''
+        Keys.reset()
+        food_row = 10
+        food_col = 10
+        world = World(food_row, food_col)
+        player = Player(world)
+        player_row = 9
+        player_col = 10
+        player_y = Tile.DIMS[1] * player_row
+        player_x = Tile.DIMS[0] * player_col
+        player.position = (player_x, player_y)
+        player.got_food = True
+
+        new_x = player.position[0]
+        new_y = player.position[1] - Player.VERT_SPEED
+        new_dead = player.dead
+        new_got_food = False
+        new_score = player.score
+
+        player.do_something()
+
+        self.assertEqual(new_x, player.position[0])
+        self.assertEqual(new_y, player.position[1])
+        self.assertEqual(new_dead, player.dead)
+        self.assertEqual(new_got_food, player.got_food)
+        self.assertEqual(new_score, player.score)
 
 if __name__ == '__main__':
     unittest.main()
