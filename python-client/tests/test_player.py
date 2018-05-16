01514bb992357335cba46004aa50483671b4d10b
Rishabh Hatgadkar <rhatgadkar@gmail.com>
2018-05-15 18:29:08 -0700

    Add player collide with enemy unit test for player

--- python-client/tests/test_player.py
+++ python-client/tests/test_player.py
@@ -5,6 +5,7 @@ from player import Player
 from stationary_tile import StationaryTile
 from moving_tile import MovingTile
 from wall import Wall
+from enemy import Enemy
 from keys import Keys
 import unittest
 
@@ -330,6 +331,65 @@ class TestPlayer(unittest.TestCase):
         new_y = player.position[1] - MovingTile.VERT_SPEED
         new_dead = player.dead
         new_got_food = False
+        new_score = player.score
+
+        player.do_something()
+
+        self.assertEqual(new_x, player.position[0])
+        self.assertEqual(new_y, player.position[1])
+        self.assertEqual(new_dead, player.dead)
+        self.assertEqual(new_got_food, player.got_food)
+        self.assertEqual(new_score, player.score)
+
+    def test_collide_enemy_from_top(self):
+        '''
+        Player collides with enemy from top, by moving down.
+        Verify that player's dead changes to True and y position changes.
+
+        Map before collision:
+        wwwwwwwwwwwwwwwwwwww
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000f00000000w
+        w000000000p00000000w
+        w000000000e00000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        w000000000000000000w
+        wwwwwwwwwwwwwwwwwwww
+        '''
+        Keys.reset()
+        food_row = 8
+        food_col = 10
+        world = World(food_row, food_col)
+        enemy_row = 10
+        enemy_col = 10
+        enemy = Enemy(world)
+        enemy.position = (StationaryTile.DIMS[1] * enemy_row,
+                          StationaryTile.DIMS[0] * enemy_col)
+        player = Player(world)
+        player_row = 9
+        player_col = 10
+        player_y = StationaryTile.DIMS[1] * (player_row + 1) - \
+                MovingTile.DIMS[1]
+        player_x = StationaryTile.DIMS[0] * player_col
+        player.position = (player_x, player_y)
+        Keys.set_down()
+
+        new_x = player.position[0]
+        new_y = player.position[1] + MovingTile.VERT_SPEED
+        new_dead = True
+        new_got_food = player.got_food
         new_score = player.score
 
         player.do_something()
